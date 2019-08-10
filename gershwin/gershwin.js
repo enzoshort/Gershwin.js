a5d00bb9f5113a3849f136f22ea5d0748064a174
enzoshort <52258826+enzoshort@users.noreply.github.com>
2019-08-10 16:40:23 -0400

    Gershwin V1.0
    The first, rather basic version of Gershwin, have fun and experiment with it!

---
+++ gershwin/gershwin.js
@@ -0,0 +1,24 @@
+(function(window) {
+    'use strict'
+    function defineGershwin() {
+        var Gershwin = {};
+        Gershwin.alert = function () {
+            alert("Howdy, it looks like Gershwin is running, so , dont feel blue!")
+        }
+       Gershwin.standardTitle = function () {
+           document.write("Gershwin Default Site!");
+       }
+
+       Gershwin.stanFooter = function () {
+           document.write("Copyright 1920 by the Deco Corp")
+       }
+           
+
+           
+       
+      return Gershwin;
+    }
+ if (typeof(Gershwin) === 'undefined') {
+     window.Gershwin = defineGershwin();
+ }
+})(window);
